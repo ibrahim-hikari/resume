f2b24b708a9c07b721e5ee9cac2734cda7a60cf2
ibraim-Ajarmeh <ajarmeh1997@gmail.com>
2020-11-03 11:16:05 +0200

    up to date

---
+++ src/components/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './reset.scss'
+
+import Header from './header/'
+import Home from './home/'
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Home />
+        </>
+    )
+}
+
+export default App;
---
+++ src/components/header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './style.scss';
+
+function Header(props) {
+    return (
+        <header>
+            <h1>
+                Ibrahim Ajarmeh
+            </h1>
+        </header>
+    )
+}
+
+export default Header;
